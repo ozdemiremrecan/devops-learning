6bcc71baa5cea68938f0d31ed3239877bc8b079b
emrecan.ozdemir <emrecan.ozdemir@gvg.com.tr>
2025-05-27 16:04:53 +0300

    initial commit

---
+++ src/test/java/com/gvg/ms/gps/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvg.ms.gps.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
